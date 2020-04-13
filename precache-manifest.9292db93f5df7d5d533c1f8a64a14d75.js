4398b52edd4ace273dd4adc36fe94819bd47ed85
Kristina Kurshakova <kurshakovakristina@gmail.com>
2020-04-13 16:02:37 +0300

    Updates

---
+++ precache-manifest.9292db93f5df7d5d533c1f8a64a14d75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f9659ef3ac13272fd8aaa39bff3ae8",
+    "url": "/pdd-exam/index.html"
+  },
+  {
+    "revision": "7fecf06984e85be5010a",
+    "url": "/pdd-exam/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "4582097f7c202cf26137",
+    "url": "/pdd-exam/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "7fecf06984e85be5010a",
+    "url": "/pdd-exam/static/js/2.4220de5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pdd-exam/static/js/2.4220de5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4582097f7c202cf26137",
+    "url": "/pdd-exam/static/js/main.dc7b36c4.chunk.js"
+  },
+  {
+    "revision": "69b09a6b0cfc16475e8e",
+    "url": "/pdd-exam/static/js/runtime-main.a8472144.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/pdd-exam/static/media/persik.4e1ec840.png"
+  }
+]);
